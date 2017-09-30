7822ca7bc02046c8f303c7369aa241bbbde9e6c7
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-09-30 21:46:22 +0900

    [Update] web-components: <check-box> を最新化
    https://github.com/tksugimoto/my-web-components/tree/f20f55242cb537bd239bf829618d01eacd20f4ce/check-box

--- chrome-extension/web-components/check-box.js
+++ chrome-extension/web-components/check-box.js
@@ -1,54 +1,83 @@
 (function (window, document) {
-	'use strict';
+	"use strict";
 	
 	const ownerDocument = document.currentScript.ownerDocument;
 	
-	const template = ownerDocument.querySelector('template').content;
+	const template = ownerDocument.querySelector("template").content;
 	
 	class CheckBoxElement extends HTMLElement {
 		constructor() {
 			super();
 
 			const shadowRoot = this.attachShadow({
-				mode: 'closed',
+				mode: "closed"
 			});
 
 			const clone = template.cloneNode(true);
 			shadowRoot.appendChild(clone);
 			
-			this._checkbox = shadowRoot.querySelector('input');
+			this._checkbox = shadowRoot.querySelector("input");
 			
-			if (this.hasAttribute('checked')) {
-				this._checkbox.checked = true;
-			}
-			if (this.hasAttribute('disabled')) {
-				this._checkbox.disabled = true;
-			}
-			
-			this._checkbox.addEventListener('change', () => {
-				this.updateCheckedAttribute();
-				const event = new window.Event('change');
-				event.checked = this._checkbox.checked;
+			this._checkbox.addEventListener("change", () => {
+				const checked = this._checkbox.checked;
+				this._updateCheckedAttribute(checked);
+				const event = new window.Event("change");
+				event.checked = checked;
 				this.dispatchEvent(event);
 			});
 		}
 
+		static get observedAttributes() {
+			return [
+				"checked",
+				"disabled",
+				"accesskey",
+			];
+		}
+
+		attributeChangedCallback(name, oldValue, newValue) {
+			if (name === "checked") {
+				const checked = newValue !== null;
+				this._checkbox.checked = checked;
+			} else if (name === "disabled") {
+				const disabled = newValue !== null;
+				this._checkbox.disabled = disabled;
+			} else if (name === "accesskey") {
+				if (newValue) {
+					this._checkbox.setAttribute("accesskey", newValue);
+				} else {
+					this._checkbox.removeAttribute("accesskey");
+				}
+			}
+		}
+
 		set checked(value) {
-			this._checkbox.checked = !!value;
-			this.updateCheckedAttribute();
+			const checked = !!value;
+			this._updateCheckedAttribute(checked);
 		}
 		get checked() {
 			return this._checkbox.checked;
 		}
 
-		updateCheckedAttribute() {
-			if (this._checkbox.checked) {
-				this.setAttribute('checked', true);
+		_updateCheckedAttribute(checked) {
+			if (checked) {
+				this.setAttribute("checked", "");
 			} else {
-				this.removeAttribute('checked');
+				this.removeAttribute("checked");
 			}
+		}
+
+		set disabled(value) {
+			if (value) {
+				this.setAttribute("disabled", "");
+			} else {
+				this.removeAttribute("disabled");
+			}
+		}
+		get disabled() {
+			return this._checkbox.disabled;
 		}
 	}
 
-	window.customElements.define('check-box', CheckBoxElement);
+	window.customElements.define("check-box", CheckBoxElement);
 })(window, document);
