408fb2a9285b788a649f04903a66c6a56edcfab3
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2020-03-20 00:30:48 +0900

    fix: chrome://など一部ページで使えない問題を修正
    無駄なurlチェックを削除
    
    【使用可能になるページ】
    chrome://
    chrome-extension://

--- chrome-extension/popup_script.js
+++ chrome-extension/popup_script.js
@@ -9,46 +9,42 @@ chrome.tabs.query({
 	currentWindow: true,
 }, tabs => {
 	const tab = tabs[0];
-	if (tab.url.match(/^(?:https?|file):/)) {
-		const data = {
-			url: tab.url,
-			title: tab.title,
-		};
-		// Firefox用
-		// Firefoxはネットワークドライブの場合/が5つ必要
-		// Chromeではfile:の後に/がいくつ並んでもOK
-		data.url = data.url.replace(/^file:[/][/]([^:/]+)[/]/, 'file://///$1/');
+	const data = {
+		url: tab.url,
+		title: tab.title,
+	};
+	// Firefox用
+	// Firefoxはネットワークドライブの場合/が5つ必要
+	// Chromeではfile:の後に/がいくつ並んでもOK
+	data.url = data.url.replace(/^file:[/][/]([^:/]+)[/]/, 'file://///$1/');
 
-		const copyCallBack = () => {
-			if (globalSettings.closeWindowAfterCopied) {
-				window.close();
-			}
-		};
+	const copyCallBack = () => {
+		if (globalSettings.closeWindowAfterCopied) {
+			window.close();
+		}
+	};
 
-		const container = document.getElementById('container');
-		templates.forEach(template => {
-			template.appendTo(container, {
-				copyCallBack,
-			});
-			template.update(data);
+	const container = document.getElementById('container');
+	templates.forEach(template => {
+		template.appendTo(container, {
+			copyCallBack,
 		});
-		globalSettings.setupOpenCopyAction(templates);
-		globalSettings.setupEnableSetting(templates);
+		template.update(data);
+	});
+	globalSettings.setupOpenCopyAction(templates);
+	globalSettings.setupEnableSetting(templates);
 
-		titleInput.value = data.title;
-		titleInput.select();
-		const change = () => {
-			setTimeout(() => {
-				data.title = titleInput.value;
-				templates.forEach(template => {
-					template.update(data);
-				});
-			}, 1);
-		};
-		titleInput.addEventListener('input', change);
-	} else {
-		document.body.innerText = i18n.getMessage('non_supported_page');
-	}
+	titleInput.value = data.title;
+	titleInput.select();
+	const change = () => {
+		setTimeout(() => {
+			data.title = titleInput.value;
+			templates.forEach(template => {
+				template.update(data);
+			});
+		}, 1);
+	};
+	titleInput.addEventListener('input', change);
 });
 
 
